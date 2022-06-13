0649027e8b449d6f657a3b94aebe0679ba77e423
xlsama <im.xlsama@gmail.com>
2022-06-13 09:16:42 +0800

    chore: delete console

--- src/utils/request.js
+++ src/utils/request.js
@@ -35,7 +35,6 @@ request.interceptors.request.use(
 // Add a response interceptor
 request.interceptors.response.use(
   function (response) {
-    console.log(response)
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response
